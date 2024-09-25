ef9a903bd8a69fa8adfb1f2fd4b75a06e07a9d59
RenanSilvaMartins <renansilva.m09@gmail.com>
2024-09-25 11:41:35 -0300

    mudei um pouco o css

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import './App.css'
 
 function App() {
   const [] = useState(0)
-  const url = ""
 
   return (
     <>
       <div className="itech">
-        <h1>App</h1>
+        <h1>iTECH </h1> 
+        <h2>O futuro na porta da sua casa</h2>
       </div>
     </>
   )

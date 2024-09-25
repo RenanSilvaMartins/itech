1b65fdff0f9dbe255a39341d130647c67d3f3f0b
RenanSilvaMartins <renansilva.m09@gmail.com>
2024-09-25 11:19:52 -0300

    comentei o index

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [] = useState(0)
+  const url = ""
 
   return (
     <>
-      <div>
-        <h1>iTECH!</h1>
-        <p>meu primeiro app</p>
+      <div className="itech">
+        <h1>App</h1>
       </div>
-      
-      
     </>
   )
 }
